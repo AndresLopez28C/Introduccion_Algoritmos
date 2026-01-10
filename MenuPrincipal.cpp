121a30647e5b7b3afb7f5027d1eb41a5f248959e
Andres <jefreyalejandro28@gmail.com>
2026-01-10 14:33:21 -0500

    menu añadido

---
+++ MenuPrincipal.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int opcion =0;
+	printf("Escriba una opcion escoger un codigo \n");
+	printf("1)Ejercicios sin funciones  \n2)Ejercicios con Funciones\n");
+	scanf("%d", &opcion);
+	
+	
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Codigos de algoritmos
+##Codigos de algoritmos
 
 Todos los archivos cpp que vez aca son programas en C o C++ los cuales incluyen elementos basicos como captura de datos, mostrar informacion y funciones
 
