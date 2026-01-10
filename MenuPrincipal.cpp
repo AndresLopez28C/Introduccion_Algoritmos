c023dc26f0f543b3c265253cd1a75f939e6a3fdf
Andres <jefreyalejandro28@gmail.com>
2026-01-10 14:49:04 -0500

    Probando nuevo menu

--- MenuPrincipal.cpp
+++ MenuPrincipal.cpp
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include<stdlib.h>
+
+void Sinfunciones();
+
 int main(){
 	int opcion =0;
 	printf("Escriba una opcion escoger un codigo \n");
 	printf("1)Ejercicios sin funciones  \n2)Ejercicios con Funciones\n");
 	scanf("%d", &opcion);
-	
+	switch (opcion){
+		case 1:
+			Sinfunciones();
+			break;
+	}
+		
+			
 	
 	return 0;
+}
+
+void Sinfunciones(){
+	int opcion=0;
+	system("cls");
+	printf("Escriba una opcion escoger un codigo \n");
+	
 }
